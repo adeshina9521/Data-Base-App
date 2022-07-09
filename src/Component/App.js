3b0d7f9c77d8cc1103f77b953f08bc79590f7f95
adeshina9521 <abdullateef.adegoke@gmail.com>
2022-07-09 17:31:28 +0100

    Die and button

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,38 +1,59 @@
 import React from "react"
 import Footer from "./Footer"
 import Die from "./Die"
+import {nanoid }from "nanoid"
 
 
 export default function App(){
 
-    const [dice, setdice] = React.useState(allNewDice())
+    const [dice, setDice] = React.useState(allNewDice())
     
     
 
     function allNewDice(){
         const newDice = [ ]
         for(let i=0; i<10; i++ ){
-            newDice.push(Math.ceil(Math.random()* 6))
+            newDice.push({value: Math.ceil(Math.random()* 6),
+                           isHeld: false,
+                            id:nanoid(),
+
+                           })
         }
+
        return newDice
     }
-    console.log(allNewDice())
 
+    function holdDice(id){
+        return console.log(id) 
+                
+    }
     const dieElement = dice.map(
         function myDie(die){
-            return  <Die value = {die}/>
+            return ( <Die 
+                key={die.id} 
+                value = {die.value} 
+                held={die.isHeld}
+                id= {die.id}
+                holdDie = {function() {
+                        return holdDice(die.id)}
+                        }
+                        />)
         }
         )
+
+    function rollDice(){
+        setDice(allNewDice())
+    }
+
+    
     return(
         <div>
-             
-
             <main>
-            <h3>Tanzie Game</h3>
-                <div className="die">
-                       {dieElement}            
-                </div>
-                <button>roll the dice</button>
+                <h3>Tanzie Game</h3>
+                    <div className="die">
+                        {dieElement}            
+                    </div>
+                    <button onClick={rollDice}className="roll">Roll</button>
             </main>
             <Footer/>
         </div>
--- src/Component/Die.js
+++ src/Component/Die.js
@@ -2,9 +2,12 @@ import React from "react";
 
 
 export default function Die(props){
+    const styles ={
+        backgroundColor: props.held ? "#59e391" : "white"
+    }
     return(
-        <div className="dies">
-            <h2 className="die-num">{props.value}</h2>
+        <div style={styles} className="dies" onClick={props.holdDie}>
+            <h2  className="die-num">{props.value}</h2>
         </div>
         
     )
